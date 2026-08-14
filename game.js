cc368620a626e5b1ecfdf0d66c25e1e3c8b524cd
bcf495y7fw-code <bcf495y7fw@privaterelay.appleid.com>
2026-08-14 11:00:42 +0700

    Update game.js

--- game.js
+++ game.js
@@ -4,6 +4,7 @@
 
 /* ---------- DOM ---------- */
 const $ = s => document.querySelector(s);
+const APP_VERSION = '1.0.4';
 const canvas = $('#game'), ctx = canvas.getContext('2d');
 const hudDepth = $('#hudDepth'), hudPearls = $('#hudPearls'),
       hudAir = $('#hudAir'), hudAirFill = $('#hudAirFill'), heartsEl = $('#hearts');
